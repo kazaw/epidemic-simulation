df33e1dd630bcaca516fc999d7bf8027fd96932d
kazaw <kacperzawadzki@gmail.com>
2020-12-18 16:42:13 +0100

    Initial commit

---
+++ src/main/java/kacper/example/epidemicsimulation/EpidemicSimulationApplication.java
@@ -0,0 +1,31 @@
+package kacper.example.epidemicsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@EnableAsync
+@SpringBootApplication
+public class EpidemicSimulationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpidemicSimulationApplication.class, args);
+    }
+
+    @Bean(name = "webExecutor")
+    public Executor asyncWebExecutor(){
+        ThreadPoolTaskExecutor webExecutor = new ThreadPoolTaskExecutor();
+        webExecutor.setCorePoolSize(5);
+        webExecutor.setMaxPoolSize(5);
+        webExecutor.setQueueCapacity(600);
+        webExecutor.setThreadNamePrefix("webService-");
+        webExecutor.initialize();
+        return webExecutor;
+    }
+
+}
+
---
+++ src/main/java/kacper/example/epidemicsimulation/WebController.java
@@ -0,0 +1,10 @@
+package kacper.example.epidemicsimulation;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+@RestController
+public class WebController{
+
+}
